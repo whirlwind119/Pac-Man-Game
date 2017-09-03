70a983f06217fe986639a7adf7bc2aad8df6b554
whirlwind119 <tim.kim1996@gmail.com>
2017-09-03 17:33:35 -0400

    Start of Game

---
+++ Pac Man/Assets/GuiScript.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GuiScript : MonoBehaviour {
+
+    float native_width = 1920;
+    float native_height = 1080;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    string calculateScore(int cur_score) {
+        string temp = NewBehaviourScript.score.ToString();
+        int temp_length = temp.Length;
+        while(temp_length < 5) {
+            temp = "0" + temp;
+            temp_length++;
+        }
+        string to_return = temp[0] + "\n" + temp[1] + "\n" + temp[2] + "\n" + temp[3] + "\n" + temp[4];
+        return to_return;
+    }
+
+    void OnGUI() {
+        float rx = Screen.width / native_width;
+        float ry = Screen.height / native_height;
+        GUI.matrix = Matrix4x4.TRS(new Vector3(0, 0, 0), Quaternion.identity, new Vector3(rx, ry, 1));
+
+        GUI.Label(new Rect(native_width / 2, native_height / 2, 400, 150), "Game\nScore\n" + calculateScore(NewBehaviourScript.score));
+
+    }
+}
---
+++ Pac Man/Assets/NewBehaviourScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public float speed = 100;
+    public string orientation;
+    public static int score = 0;
+
+
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+        orientation = "right";
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+            orientation = "up";
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+            orientation = "left";
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+            orientation = "down";
+        }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+            orientation = "right";
+        }
+        
+        if (orientation == "up") {
+            gameObject.transform.eulerAngles = new Vector3(gameObject.transform.eulerAngles.x, gameObject.transform.eulerAngles.y, 90f);
+            rb.velocity = Vector2.up * speed * Time.deltaTime;
+        }
+        if (orientation == "left") {
+            gameObject.transform.eulerAngles = new Vector3(gameObject.transform.eulerAngles.x, gameObject.transform.eulerAngles.y, 180f);
+            rb.velocity = Vector2.left * speed * Time.deltaTime;
+        }
+        if (orientation == "down") {
+            gameObject.transform.eulerAngles = new Vector3(gameObject.transform.eulerAngles.x, gameObject.transform.eulerAngles.y, 270f);
+            rb.velocity = Vector2.down * speed * Time.deltaTime;
+        }
+        if (orientation == "right") {
+            gameObject.transform.eulerAngles = new Vector3(gameObject.transform.eulerAngles.x, gameObject.transform.eulerAngles.y, 0f);
+            rb.velocity = Vector2.right * speed * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D c) {
+        if (c.gameObject.tag == "coin") {
+            Destroy(c.gameObject);
+            score++;
+        }
+    }
+
+
+
+}
